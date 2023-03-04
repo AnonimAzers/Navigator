f9ae64d6d9c47081cdb980fd7eb2b9da246663cd
maksi <maksi@MSI>
2023-03-04 16:05:35 +0300

    Добавьте файлы проекта.

---
+++ Navigator/DrawRoad.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Navigator
+{
+    enum RoadType
+    {
+        CAR_ROAD = 0,
+        CYRCLE_ROAD,
+        TRAIN_ROAD,
+        WALKER_ROAD
+    }
+
+    internal class DrawRoadClass
+    {
+        private RoadType _RealRoadType;
+        private Dictionary<Ellipse, int[]> _Nodes; // Вершины
+        private Dictionary<Ellipse[], int> _RoadWeight; // Веса
+        private Dictionary<RoadType, string> _RoadColor;
+        private MainWindow _Window { get; set; }
+
+        public DrawRoadClass(MainWindow window)
+        {
+            _RealRoadType = RoadType.CAR_ROAD;
+            _Nodes = new Dictionary<Ellipse, int[]>();
+            _RoadWeight = new Dictionary<Ellipse[], int>();
+            _RoadColor = new Dictionary<RoadType, string>()
+            {
+                {RoadType.CAR_ROAD, "#FFFF0000" },
+                {RoadType.CYRCLE_ROAD, "#FFFFFF00" },
+                {RoadType.TRAIN_ROAD, "#FF00FF00" },
+                {RoadType.WALKER_ROAD, "#FF00FFFF" }
+            };
+            _Window = window;
+        }
+
+        public void DrawRoad(int FirstNode, int SecondNode)
+        {
+
+        }
+
+        public void SetNode(int posX, int posY)
+        {
+
+        }
+
+        public void SetRoadType(RoadType TYPE) {
+            _RealRoadType = TYPE;
+        }
+
+    }
+}
---
+++ Navigator/EventHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Navigator
+{
+    internal class Events
+    {
+        private MainWindow _Window { get; set; }
+        private DrawRoadClass _DrawRoadClass;
+
+        public Events(MainWindow Window)
+        {
+            this._Window = Window;
+            _DrawRoadClass = new DrawRoadClass(Window);
+        }
+
+        public void TopRootMouseDown_Event(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _Window.DragMove();
+            }
+        }
+
+        public void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            _Window.Close();
+        }
+
+        public void RoadButtonClick_Event(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Content.ToString())
+            {
+                case "Car":
+                    _DrawRoadClass.SetRoadType(RoadType.CAR_ROAD);
+                    break;
+                case "Cyrcle":
+                    _DrawRoadClass.SetRoadType(RoadType.CYRCLE_ROAD);
+                    break;
+                case "Train":
+                    _DrawRoadClass.SetRoadType(RoadType.TRAIN_ROAD);
+                    break;
+                case "Walker":
+                    _DrawRoadClass.SetRoadType(RoadType.WALKER_ROAD);
+                    break;
+
+            }
+        }
+
+        public void MapClick_Event(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
